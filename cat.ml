5ac183dbaa99175accd05a3fd1447ef73de4d806
Trevor Summers Smith <trevor@knewton.com>
2014-09-15 20:41:46 -0400

    Add the UV_FS_PRIVATE_FIELDS fields to uv_fs.
    Added the fields for unix.
    
    This required adding a few more types. Did that.
    This still segfaults when trying to read the path variable of the uv_fs
    struct.
    
    Working with version 0.11.29 of libuv.
    
    Size of struct is 408. C-compiled version is same size.

--- cat.ml
+++ cat.ml
@@ -2,13 +2,24 @@ open Ctypes
 open Foreign
 
 let open_callback (req : Uv.uv_fs structure ptr) : unit =
-  let result = getf (!@req) Uv.result in
-  Printf.printf "Got: %u\n" result
+  (*let result = getf (!@req) Uv.result in*)
+  let path = getf (!@req) Uv.path in
+  Printf.printf "Got:  %s%!\n" path
+  (*Printf.printf "Got: %d and %s%!\n" (Signed.Long.to_int result) path*)
+
+let ok_callback req =
+  let stat = getf (!@req) Uv.statbuf in
+  let size : int64 = Unsigned.UInt64.to_int64 (getf stat Uv.st_size) in
+  let result = coerce PosixTypes.ssize_t int64_t (getf (!@req) Uv.result) in
+  Printf.printf "ok! size: %u\n" (Int64.to_int size);
+  Printf.printf "result: %d\n" (Int64.to_int result)
 
 let () =
   let req : Uv.uv_fs structure = make Uv.uv_fs in
   let req_ptr = addr req in
+  (*let ret1 = Uv.uv_fs_open looper req_ptr "cat.c" 0 0 open_callback in*)
   let looper = Uv.uv_default_loop () in
-  let i = Uv.uv_fs_open looper req_ptr "cat.c" 0 0 open_callback in
+  let ret1 = Uv.uv_fs_stat looper req_ptr "cat.c" open_callback in
   let i = Uv.uv_run looper 0 in
+  Printf.printf "open: %d\n" ret1;
   Printf.printf "ok: %u\n" i
--- uv.ml
+++ uv.ml
@@ -16,6 +16,37 @@ let uv_run =
   foreign "uv_run" (uv_loop @-> int @-> returning int)
 (* TODO should be an enum not an int for second arg *)
 
+(* threadpool.h -- uv__work
+struct uv__work {
+  void ( *work)(struct uv__work *w);
+  void ( *done)(struct uv__work *w, int status);
+  struct uv_loop_s* loop;
+  void* wq[2];
+};
+*)
+type uv__work (* we'll keep their convention of 2 underscores? *)
+let uv__work : uv__work structure typ = structure "uv__work"
+let uv__work_work_cb = ptr uv__work @-> returning void
+let uv__work_done_cb = ptr uv__work @-> int @-> returning void
+
+let uv__work_work = field uv__work "uv__work_work" (funptr uv__work_work_cb)
+let uv__work_done = field uv__work "uv__work_done" (funptr uv__work_done_cb)
+let uv__work_loop = field uv__work "uv__work_loop" uv_loop
+let uv__work_wq = field uv__work "uv__work_wq" (array 2 (ptr void))
+let () = seal uv__work
+
+(* buffer -- TODO platform dependent uv-unix.h
+typedef struct uv_buf_t {
+  char* base;
+  size_t len;
+} uv_buf_t;
+*)
+type uv_buf
+let uv_buf : uv_buf structure typ = structure "uv_buf"
+let uv_buf_base = field uv_buf "uv_buf_base" string
+let uv_buf_len = field uv_buf "uv_buf_len" size_t
+let () = seal uv_buf
+
 (* timespec *)
 
 type uv_timespec
@@ -90,23 +121,54 @@ struct uv_fs_s {
   const char* path;
   uv_stat_t statbuf;  /* Stores the result of uv_fs_stat() and uv_fs_fstat(). */
   UV_FS_PRIVATE_FIELDS
+
+// UV_FS_PRIVATE_FIELDS for unix:
+ const char *new_path;                                                       \
+  uv_file file;                                                               \
+  int flags;                                                                  \
+  mode_t mode;                                                                \
+  unsigned int nbufs;                                                         \
+  uv_buf_t* bufs;                                                             \
+  off_t off;                                                                  \
+  uv_uid_t uid;                                                               \
+  uv_gid_t gid;                                                               \
+  double atime;                                                               \
+  double mtime;                                                               \
+  struct uv__work work_req;                                                   \
+  uv_buf_t bufsml[4];
 };
 
 *)
 let data = field uv_fs "data" (ptr void)
-let uv_req_type = field uv_fs "type" int (* TODO enum *)
+let uv_req_type = field uv_fs "type" long (* TODO enum *)
 let active_queue = field uv_fs "active_queue" (array 2 (ptr void))
 let reserved = field uv_fs "reserved" (array 4 (ptr void))
 (* TODO UV_REQ_PRIVATE_FIELDS currently not defined for any platforms *)
-let fs_type = field uv_fs "fs_type" int (* TODO enum *)
+let fs_type = field uv_fs "fs_type" long (* TODO enum *)
 let uv_fs_uv_loop = field uv_fs "uv_fs_uv_loop" uv_loop (* TODO naming *)
 let cb = field uv_fs "cb" (funptr uv_fs_cb) (* TODO I think this is just type uv_fs_cb and NOT funptr? *)
-let result = field uv_fs "result" int (* TODO should be ssize_t *)
+let result = field uv_fs "result" PosixTypes.ssize_t
 let uv_fs_ptr = field uv_fs "uv_fs_ptr" (ptr void)
 let path = field uv_fs "path" string
 let statbuf = field uv_fs "statbuf" uv_stat
 (* TODO UV_FS_PRIVATE_FIELDS currently not defined for any platforms *)
+let new_path = field uv_fs "new_path" string
+let file = field uv_fs "file" int (* TODO type is platform dependent *)
+let flags = field uv_fs "flags" int
+let mode = field uv_fs "mode" PosixTypes.mode_t
+let nbufs = field uv_fs "nbufs" uint
+let bufs = field uv_fs "bufs" (ptr uv_buf)
+let off = field uv_fs "off" PosixTypes.off_t
+let uid = field uv_fs "uid" PosixTypes.uid_t (* TODO platform specific uv-unix.h*)
+let gid = field uv_fs "gid" PosixTypes.gid_t (* TODO platform specific uv-unix.h*)
+let atime = field uv_fs "atime" double
+let mtime = field uv_fs "mtime" double
+let work_req = field uv_fs "work_req" uv__work
+let bufsml = field uv_fs "bufsml" (array 4 uv_buf)
 let () = seal uv_fs
 
 let uv_fs_open =
   foreign "uv_fs_open" (uv_loop @-> ptr uv_fs @-> string @-> int (* flags *) @-> int (* mode *) @-> funptr uv_fs_cb @-> returning int)
+
+let uv_fs_stat =
+  foreign "uv_fs_stat" (uv_loop @-> ptr uv_fs @-> string @-> funptr uv_fs_cb @-> returning int)
