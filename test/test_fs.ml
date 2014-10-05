da89c15bc34aa37ab53bb0005a5d301309687ce2
Zachary Newman <znewman01@gmail.com>
2014-10-04 21:01:25 -0400

    [fs] Add test for fs_read

--- test/test_fs.ml
+++ test/test_fs.ml
@@ -30,9 +30,37 @@ let test_blocking_fs_stat _ =
   assert_equal stats.st_size (Int64.of_int 3);
   assert_equal (Uv.FS.path fs) filename
 
+let test_fs_read _ =
+  let test_string = "test" in
+  let fd = ref 0 in
+  let rec open_callback request =
+    fd := Int64.to_int (Uv.FS.result request);
+    let _ =
+      Uv.FS.read !fd ~cb:read_callback in ()
+  and read_callback request =
+    let buf = Uv.FS.buf request in
+    let _ = Uv.FS.close !fd in
+    assert_equal (Uv.FS.string_of_iobuf buf) "test"
+  in
+  let filename = mk_tmpfile test_string in
+  let _ = Uv.FS.openfile filename 0 ~cb:open_callback in
+  let _ = Uv.Loop.run (Uv.Loop.default_loop ()) RunDefault in ()
+
+let test_blocking_fs_read _ =
+  let test_string = "test" in
+  let filename = mk_tmpfile test_string in
+  let open_request = Uv.FS.openfile filename 0 in
+  let _ = Uv.Loop.run (Uv.Loop.default_loop ()) RunDefault in
+  let fd = Int64.to_int (Uv.FS.result open_request) in
+  let read_request = Uv.FS.read fd in
+  let buf = Uv.FS.buf read_request in
+  assert_equal (Uv.FS.string_of_iobuf buf) "test"
+
 let suite =
   "fs_suite">:::
     [
       "fs_stat">::test_fs_stat;
       "blocking_fs_stat">::test_blocking_fs_stat;
+      "fs_read">::test_fs_read;
+      "blocking_fs_read">::test_blocking_fs_read;
     ]
