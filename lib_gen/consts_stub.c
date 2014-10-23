51f3b1021eab1ca6fbda1ff50ac45bba338896f7
Zachary Newman <znewman01@gmail.com>
2014-10-22 23:47:22 -0400

    constants: WIP

---
+++ lib_gen/consts_stub.c
@@ -0,0 +1,65 @@
+/* Generate a list of constant definitions.
+ *
+ * Heavily indebted to
+ * https://github.com/dbuenzli/tsdl/blob/master/support/consts_stub.c .
+ */
+
+
+#include <caml/mlvalues.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <uv.h>
+
+void let (FILE *fd, const char *symb) {
+    int i;
+    fprintf (fd, "let ");
+    for (i = 0; i < strlen(symb); i++) {
+        fprintf (fd, "%c", tolower (symb[i]));
+    }
+}
+
+void str_v (FILE *fd, char *symb, const char *value) {
+    let (fd, symb); fprintf (fd, " = \"%s\"\n", value);
+}
+
+void int_v (FILE *fd, char *symb, int value) {
+    let (fd, symb); fprintf (fd, " = %d\n", value);
+}
+
+void int_vx (FILE *fd, char *symb, int value) {
+    let (fd, symb); fprintf (fd, " = 0x%X\n", value);
+}
+
+void int32_v (FILE *fd, char *symb, int32_t value) {
+    let (fd, symb); fprintf (fd, " = 0x%Xl\n", value);
+}
+
+void consts (FILE *fd) {
+
+    #define int_v(e) int_v(fd, "" # e, (int)e)
+    #define int_vx(e) int_vx(fd, "" # e, (int)e)
+    #define int32_v(e) int32_v(fd, "" # e, (int32_t)e)
+    #define str_v(e) str_v(fd, "" # e, (const char *)e)
+
+}
+
+CAMLprim value output_consts (value fname) {
+    char *outf = String_val (fname);
+    FILE *fd;
+    if (strlen(outf) == 0) {
+        fd = stdout;
+    } else {
+        fd = fopen (outf, "w");
+        if (!fd) {
+            perror(outf); exit (1);
+        }
+    }
+    consts(fd);
+    fflush(fd);
+    if (fd != stdout) {
+        fclose (fd);
+    }
+    return Val_unit;
+}
--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,5 +1,18 @@
 open Ocamlbuild_plugin;;
 
+let uv_consts_build () =
+    dep [ "link"; "ocaml"; "link_consts_stub" ] [ "lib_gen/consts_stub.o" ];
+    dep [ "uv_consts" ] [ "src/uv_consts.ml" ];
+    rule "uv_consts: consts.byte -> uv_consts.ml"
+    ~dep:"lib_gen/consts.byte"
+    ~prod:"src/uv_consts.ml"
+    begin fun env build ->
+        let enums = env "support/consts.byte" in
+        let prod = env "src/uv_consts.ml" in
+        Cmd (S [A enums; A prod])
+    end;
+;;
+
 dispatch begin function
   | Before_options ->
       Options.use_ocamlfind := true
@@ -12,6 +25,7 @@ dispatch begin function
       flag ["compile"; "use_ctypes_c_headers"] (S[A"-I"; Px (ctypes.Findlib.location ^ "/..")]);
       flag ["ocaml"; "compile"; "use_libuv_generated_stubs"] (S[Px"src/libuv_generated_stubs.o"]);
       dep ["ocaml"; "use_libuv_generated_stubs"] ["src/libuv_generated_stubs.o"];
-      flag ["ocaml"; "link"; "use_libuv"] (S[A"-cclib"; A"-luv"])
+      flag ["ocaml"; "link"; "use_libuv"] (S[A"-cclib"; A"-luv"]);
+      uv_consts_build ()
   | _ -> ()
 end
