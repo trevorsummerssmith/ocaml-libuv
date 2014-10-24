7f265d63081a7289a227e0068e9ad6b3b0a5868c
Trevor Summers Smith <trevorsummerssmith@gmail.com>
2014-10-23 22:06:22 -0400

    WIP Add example accessor function
    We're going to change how we interact with the libuv structs:
    instead of specifying the struct fields to determine memory layout
    we will determine the size of the struct and then allocate that many
    bytes. This will allow us to make less changes as libuv changes.
    Also it is helpful because it is cross platform.
    
    However, if we do this, we need a way to access the public struct
    fields. This commit shows the way we'll be doing this: a c/h file
    pair that reference the struct's field. To keep things low on brittle
    code we're generating these two files from libuv_accessor_gen.ml

--- test/test_handle.ml
+++ test/test_handle.ml
@@ -5,19 +5,10 @@ open Uv
 let test_accessors () =
   let loop = Loop.default_loop () in
   let handle = TCP.init ~loop:loop () in
-  let handle2 = TCP.init ~loop:loop () in
-  Printf.printf "loop: 0x%Lx\nHandle: 0x%Lx\nHandle: 0x%Lx\n" (Loop.ok loop) (Loop.ok (Handle.loop handle)) (Loop.ok (Handle.loop handle2));
-  assert_equal loop (Handle.loop handle);
-  assert_equal (Handle.loop handle) (Handle.loop handle2)
-
-let test_loop () =
-  let loop = Loop.default_loop () in
-  let loop2 = Loop.default_loop () in
-  assert_equal loop loop2
+  assert_equal loop (Handle.loop handle)
 
 let suite =
   "handle suite">:::
   [
     "accessors">::test_accessors;
-    "loop">::test_loop
   ]
