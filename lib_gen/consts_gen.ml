e431bf86fef3d10e8a6c8e578d608d4e35c77047
Trevor Summers Smith <trevorsummerssmith@gmail.com>
2014-11-08 08:24:45 -0500

    Consts now working!
    Previous WIP commit didn't actually compile anything. Now this does.
    * Added a test to ensure everything works
    * Changed tag names for the build to be more uniform

--- lib_gen/consts_gen.ml
+++ lib_gen/consts_gen.ml
@@ -1,3 +1,11 @@
+(*
+   This file runs the consts_stubs generation.
+   This file only exists because it is easier to have
+   ocamlbuild generate an ocaml executable. If I could
+   figure out how to make it generate a c executable
+   then this file would not exist.
+*)
+
 external output_consts : string -> unit = "output_consts"
 
 let main () =
