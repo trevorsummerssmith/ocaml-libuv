16009f43711c380b1ca3236cf5a48e53b617d6ee
Trevor Summers Smith <trevor@knewton.com>
2014-11-14 19:25:12 -0800

    Lifecycle code working and ready to go with FS
    This correctly stores both the callback, and data allocated
    by OCaml. Ditched refcounts (because we aren't counting anymore)
    in favor of a straightforward 'store unique id with the object'
    module we're calling 'coat check'.
    
    * This is just implemented in FS right now. Will roll out to the
      rest of the stuff once we implement the error handling.
    * Minor cleanup in test_lifecycle

--- test/test_runner.ml
+++ test/test_runner.ml
@@ -6,6 +6,7 @@ let () =
     Test_handle.suite;
     Test_lifecycle.suite;
     Test_consts.suite;
+    Test_coat_check.suite;
   ] in
   let _ = List.map (fun s -> run_test_tt_main s) suites in
   (* We don't need the results *)
