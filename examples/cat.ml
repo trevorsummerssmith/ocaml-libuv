ea54c190db86bd8bdc419aa2714b6115df2755f4
Trevor Summers Smith <trevor@knewton.com>
2014-11-17 17:21:42 -0500

    Update examples to use new FS result type

--- examples/cat.ml
+++ examples/cat.ml
@@ -2,22 +2,26 @@ open Uv
 
 let fd_ref = ref 0
 
+let ok_exn = function
+    Uv.FS.Ok i -> i
+  | _ -> failwith "Error"
+
 let rec read_callback fs =
-  match FS.result fs with
-    r when r < Int64.zero -> Printf.printf "ok"
-  | r when r = Int64.zero -> let _ = FS.close !fd_ref in ()
+  match ok_exn (FS.result fs) with
+    r when r < 0 -> Printf.printf "ok"
+  | r when r = 0 -> let _ = FS.close !fd_ref in ()
   | r ->
      let buf = FS.buf fs in
-     let buf2 = Bigarray.Array1.sub buf 0 (Int64.to_int r) in
+     let buf2 = Bigarray.Array1.sub buf 0 r in
      let _ = FS.write 1 buf2 ~cb:write_callback in ()
 and write_callback fs =
-  if FS.result fs < Int64.zero then
+  if ok_exn(FS.result fs) < 0 then
     Printf.fprintf stderr "Write error\n"
   else
     let _ = FS.read !fd_ref ~cb:read_callback in ()
 
 let open_callback fs =
-  let fd = Int64.to_int (FS.result fs) in
+  let fd = ok_exn (FS.result fs) in
   let _ = fd_ref := fd in
   let _ = FS.read fd ~cb:read_callback in
   ()
